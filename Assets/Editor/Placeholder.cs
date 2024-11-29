ff3690c7370317c09f8f4767f9ec0ed70a2f17a2
Ítalo <italo.ts4895@gmail.com>
2024-11-29 15:40:23 -0300

    Project packages setup

---
+++ Assets/Editor/Placeholder.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NotAVampireSurvivor.Editor
+{
+    public class Placeholder : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
